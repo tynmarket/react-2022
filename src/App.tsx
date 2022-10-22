a10487cbe5f501e8f7982fe7648460fc7d84ac2b
tynmarket <tynmarket@gmail.com>
2022-10-22 23:50:51 +0900

    絶対パス

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
-import Layout from './Layout'
+import Layout from '@/Layout'
 import Top from './Top'
 
 function App() {
