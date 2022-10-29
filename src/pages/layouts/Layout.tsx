635094fa189b37a9a2c26d2d5e797057b2b4ac64
tynmarket <tynmarket@gmail.com>
2022-10-29 13:21:19 +0900

    cssを一部置き換え

--- src/pages/layouts/Layout.tsx
+++ src/pages/layouts/Layout.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { Outlet } from "react-router-dom";
+import { css } from "@emotion/react"
 
 const Layout = () => {
   return (
     <>
-      <div style={{ color: 'red' }}>レイアウト</div>
+      <div
+        style={{ color: 'red' }}
+        css={layout}
+      >
+        レイアウト
+      </div>
       <Outlet />
     </>
   );
 }
 
 export default Layout;
+
+const layout = css`
+  font-weight: 700;
+`;
--- src/pages/top/Top.tsx
+++ src/pages/top/Top.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { css } from "@emotion/react"
 
 import { useStore } from "@/libs/store";
 
